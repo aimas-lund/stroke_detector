62d66bc36a63b03f0d87563db97be2c94ef26d9c
joshuakim0724 <joshuakim0724@yahoo.com>
2019-11-30 20:47:11 +0900

    Added ForegroundService & Buttons to start/end service

---
+++ app/src/main/java/com/passivestrokedetector/ForegroundService.java
@@ -0,0 +1,69 @@
+package com.passivestrokedetector;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Build;
+import android.os.IBinder;
+import android.widget.Toast;
+
+import androidx.core.app.NotificationCompat;
+
+public class ForegroundService extends Service {
+
+    private static final int NOTIF_ID = 1;
+    public static final String CHANNEL_ID = "ForegroundServiceChannel";
+
+    public ForegroundService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        startForeground(intent);
+
+        return START_NOT_STICKY;
+//        return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel serviceChannel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Foreground Service Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(serviceChannel);
+        }
+    }
+    private void startForeground(Intent intent) {
+        String input = intent.getStringExtra("inputExtra");
+        createNotificationChannel();
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                0, notificationIntent, 0);
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("Foreground Service")
+                .setContentText(input)
+//                .setSmallIcon(R.drawable.ic_stat_name)
+                .setContentIntent(pendingIntent)
+                .build();
+
+        startForeground(1, notification);
+    }
+}
--- app/src/main/java/com/passivestrokedetector/MainActivity.java
+++ app/src/main/java/com/passivestrokedetector/MainActivity.java
@@ -1,12 +1,18 @@
 package com.passivestrokedetector;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private int REQUEST_CODE_PERMISSIONS = 10;
 
@@ -23,6 +29,38 @@ public class MainActivity extends AppCompatActivity {
 
         ImageAnalyzer imageAnalyzer = new ImageAnalyzer(this);
 
+        Button startBtn = findViewById(R.id.buttonStartService);
+        Button stopBtn = findViewById(R.id.buttonStopService);
+
+        startBtn.setOnClickListener(this);
+        stopBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.buttonStartService: {
+                Toast.makeText(this, "Service has started", Toast.LENGTH_SHORT).show();
+                startService();
+                break;
+            }
+            case R.id.buttonStopService: {
+                Toast.makeText(this, "Service has ended", Toast.LENGTH_SHORT).show();
+                stopService();
+                break;
+            }
+        }
+    }
+    private void startService() {
+        Intent serviceIntent = new Intent(this, ForegroundService.class);
+        serviceIntent.putExtra("inputExtra", "Foreground Stroke Detector in Android");
+
+        ContextCompat.startForegroundService(this, serviceIntent);
+    }
+
+    public void stopService() {
+        Intent serviceIntent = new Intent(this, ForegroundService.class);
+        stopService(serviceIntent);
     }
 }
 
