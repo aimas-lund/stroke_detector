d5f8bcaa2874e9dc2776d4ff04e265fb716c8009
AimasLund <aimas.lund@gmail.com>
2019-11-30 16:15:57 +0900

    CReated Image Analyzer Class

--- app/src/main/java/com/passivestrokedetector/MainActivity.java
+++ app/src/main/java/com/passivestrokedetector/MainActivity.java
@@ -2,12 +2,26 @@ package com.passivestrokedetector;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.Manifest;
+
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private int REQUEST_CODE_PERMISSIONS = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseVisionFaceDetectorOptions highAccuracyOpts =
+                new FirebaseVisionFaceDetectorOptions.Builder()
+                        .setPerformanceMode(FirebaseVisionFaceDetectorOptions.FAST)
+                        .setLandmarkMode(FirebaseVisionFaceDetectorOptions.ALL_LANDMARKS)
+                        .build();
+
     }
 }
