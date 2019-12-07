fd3cb7606d9a6aaf0e68a7c5ffa8d94220bce539
AimasLund <aimas.lund@gmail.com>
2019-12-07 16:30:29 +0900

    Implemented Instance creation functions

---
+++ app/src/main/java/com/passivestrokedetector/FacialFeature.java
@@ -0,0 +1,5 @@
+package com.passivestrokedetector;
+
+public enum FacialFeature {
+    LEFT_EYE, RIGHT_EYE, LOWER_LIP, UPPER_LIP
+}
--- app/src/main/java/com/passivestrokedetector/StrokeClassifier.java
+++ app/src/main/java/com/passivestrokedetector/StrokeClassifier.java
@@ -1,17 +1,29 @@
 package com.passivestrokedetector;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import weka.core.FastVector;
+import weka.core.Instance;
 import weka.core.Instances;
+import weka.core.Attribute;
 
 public class StrokeClassifier {
 
-    private int numFeatures = 4;
+    private int numFeatures = 8;
     private int numClasses = 2;
-    private List<Float> listFeature;
-    private List<Float> listClass;
+    private List<String> listFeatureLeftEye = initList("le");
+    private List<String> listFeatureRightEye = initList("re");
+    private List<String> listFeatureLowerLip = initList("ll");
+    private List<String> listFeatureUpperLip = initList("ul");
+    private List<List<String>> allFeatures =
+            Arrays.asList(
+            listFeatureLeftEye,
+            listFeatureRightEye,
+            listFeatureLowerLip,
+            listFeatureUpperLip);
+    private List<String> listClass = Arrays.asList("Normal", "Drooping");
 
     private Instances instances = createEmptyInstances();
 
@@ -19,20 +31,54 @@ public class StrokeClassifier {
         /* Create an empty list of instances */
         FastVector attrs = new FastVector();
 
-        for (f in listFeature) {
-            attrs.addElement(Attribute(f))
+        for (List<String> list : allFeatures) {
+            for (String f : list) {
+                attrs.addElement(new Attribute(f));
+            }
         }
 
-        val classes = FastVector()
-        for (c in listClass) {
-            classes.addElement(c)
-        }
-        attrs.addElement(Attribute("label", classes))
+        FastVector classes = new FastVector();
 
-        return Instances("myInstances", attrs, 10000)
+        for (String c : listClass) {
+            classes.addElement(c);
+        }
+        attrs.addElement(new Attribute("label", classes));
+
+        return new Instances("modelInstances", attrs, 10000);
     }
 
+    /*
+    Be sure that the lists contains values from all the facial features in the following order:
+    left eye, right eye, lower lip, upper lip
+     */
+    public Instance createInstance(List<String> attrs, List<Double> values, String className) {
+        Attribute attrClass = instances.attribute("label");
+        Instance instance = new Instance((numFeatures + 1)*allFeatures.size());
 
+        for (int j = 0; j < attrs.size(); j++) {
+            Attribute attrInstance = instances.attribute(attrs.get(j));
+            instance.setValue(attrInstance, values.get(j));
+        }
+        instance.setValue(attrClass, className);
 
+        return instance;
+    }
+
+    /*
+    =====================================
+    AUXILIARY FUNCTIONS
+    =====================================
+     */
+
+    private List<String> initList(String featureName) {
+
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < numFeatures; i++) {
+            list.add(featureName + i);
+        }
+
+        return list;
+    }
 
 }
